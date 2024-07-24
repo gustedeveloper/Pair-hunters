a271b0c50b6b80e67ecd39e8778d766f58abb608
Guste Gaubaite <219.guste@gmail.com>
2024-07-24 10:37:36 +0200

    Implementada función para mostrar la imagen de 2 dos cartas y si el src de imagen no es igual, voltearlas

--- Prueba de concepto 4/src/main.ts
+++ Prueba de concepto 4/src/main.ts
@@ -1,2 +1,49 @@
 import "./style.css";
 
+interface CardsData {
+    idCard: number,
+    img: string
+  }
+  
+  const cardsData : CardsData[] = [
+    {
+      idCard: 1,
+      img: "https://github.com/Lemoncode/fotos-ejemplos/blob/main/memo/1.png?raw=true"
+    },
+    {
+      idCard: 2,
+      img: "https://github.com/Lemoncode/fotos-ejemplos/blob/main/memo/2.png?raw=true"
+    }
+  ]
+
+  const cardsDivElements = document.querySelectorAll(".card");
+  const cardsImgElements = document.querySelectorAll(".card-img");
+
+  const cardsDiv = Array.from(cardsDivElements);
+  const cardsImg = Array.from(cardsImgElements);
+
+  let clickedCardsImg : HTMLImageElement[] = [];
+
+  cardsDiv.forEach((card) => {
+      card.addEventListener("click", () => {
+        const clickedCardId = cardsDiv.indexOf(card);
+        const imgElement = cardsImg[clickedCardId];
+        if (imgElement instanceof HTMLImageElement) {
+          imgElement.src = cardsData[clickedCardId].img;
+          clickedCardsImg.push(imgElement);
+      }
+
+        const firstCardImg = clickedCardsImg[0];
+        const secondCardImg = clickedCardsImg[1];
+
+        if (firstCardImg instanceof HTMLImageElement && secondCardImg instanceof HTMLImageElement) {
+          if (firstCardImg.src !== secondCardImg.src) {
+            setTimeout(() => {
+              clickedCardsImg.forEach(img => {
+                img.src = "";
+              })
+            }, 1500);
+          }
+        }
+      });
+  });
