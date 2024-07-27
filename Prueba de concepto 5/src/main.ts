f8b17de1de27b81b7095bd65e12cf20f10fb3405
Guste Gaubaite <219.guste@gmail.com>
2024-07-27 12:02:37 +0200

    Refactor: implementada función que itera sobre array de cartas para crear div por cada carta, que al clickar cada div se genere la imagen correspondiente al array de cartas

--- Prueba de concepto 5/src/main.ts
+++ Prueba de concepto 5/src/main.ts
@@ -1,45 +1,79 @@
 import "./style.css";
 
-const cards : number[] = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5];
+document.addEventListener("DOMContentLoaded", () => {
+  createDivPerCard();
+});
 
-interface CardsData {
-    idCard: number,
-    img: string
+interface Cards {
+  idCard: number;
+  img: string;
 }
 
-const cardsData : CardsData[] = [
-    {
-        idCard: 0,
-        img: "https://github.com/Lemoncode/fotos-ejemplos/blob/main/memo/1.png?raw=true"
-    },
-    {
-        idCard: 1,
-        img: "https://github.com/Lemoncode/fotos-ejemplos/blob/main/memo/2.png?raw=true"
-    },
-    {   
-        idCard: 2,
-        img: "https://github.com/Lemoncode/fotos-ejemplos/blob/main/memo/3.png?raw=true"
-    },
-    {
-        idCard: 3,
-        img: "https://github.com/Lemoncode/fotos-ejemplos/blob/main/memo/4.png?raw=true"
-    }
-]
+const cards: Cards[] = [
+  {
+    idCard: 1,
+    img: "https://github.com/Lemoncode/fotos-ejemplos/blob/main/memo/1.png?raw=true",
+  },
+  {
+    idCard: 2,
+    img: "https://github.com/Lemoncode/fotos-ejemplos/blob/main/memo/2.png?raw=true",
+  },
+  {
+    idCard: 3,
+    img: "https://github.com/Lemoncode/fotos-ejemplos/blob/main/memo/3.png?raw=true",
+  },
+  {
+    idCard: 4,
+    img: "https://github.com/Lemoncode/fotos-ejemplos/blob/main/memo/4.png?raw=true",
+  },
+  {
+    idCard: 5,
+    img: "https://github.com/Lemoncode/fotos-ejemplos/blob/main/memo/5.png?raw=true",
+  },
+  {
+    idCard: 6,
+    img: "https://github.com/Lemoncode/fotos-ejemplos/blob/main/memo/6.png?raw=true",
+  },
+  {
+    idCard: 1,
+    img: "https://github.com/Lemoncode/fotos-ejemplos/blob/main/memo/1.png?raw=true",
+  },
+  {
+    idCard: 2,
+    img: "https://github.com/Lemoncode/fotos-ejemplos/blob/main/memo/2.png?raw=true",
+  },
+  {
+    idCard: 3,
+    img: "https://github.com/Lemoncode/fotos-ejemplos/blob/main/memo/3.png?raw=true",
+  },
+  {
+    idCard: 4,
+    img: "https://github.com/Lemoncode/fotos-ejemplos/blob/main/memo/4.png?raw=true",
+  },
+  {
+    idCard: 5,
+    img: "https://github.com/Lemoncode/fotos-ejemplos/blob/main/memo/5.png?raw=true",
+  },
+  {
+    idCard: 6,
+    img: "https://github.com/Lemoncode/fotos-ejemplos/blob/main/memo/6.png?raw=true",
+  },
+];
 
+const cardContainer = document.getElementById("card-container");
 
-
-cards.forEach((card, index) => {
-    const divCard = document.getElementById(`card${index}`);
-    
-    if (divCard && divCard instanceof HTMLDivElement) {
-      divCard.addEventListener("click", () => {
-        const imgCard = document.getElementById(`img${index}`);
-
-      if (imgCard && imgCard instanceof HTMLImageElement) {
-        imgCard.src = cardsData[card].img;
-        console.log(card);
-      }
-
-      });
-    }
+const createDivPerCard = (): void => {
+  cards.forEach((card, index) => {
+    const divCard = document.createElement("div");
+    divCard.classList.add("card");
+    divCard.id = `${index}`;
+    cardContainer?.appendChild(divCard);
+    divCard.addEventListener("click", () => {
+      const cardImg = document.createElement("img");
+      divCard.appendChild(cardImg);
+      cardImg.id = `${index}`;
+      cardImg.src = card.img;
+    });
+    return divCard;
   });
+};
